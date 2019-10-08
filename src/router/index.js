59a7e03b53c3b0c6bbce1624a012971883ac41d1
jack <1339326122@qq.com>
2019-10-08 15:33:48 +0800

    \\

--- src/router/index.js
+++ src/router/index.js
@@ -23,9 +23,7 @@ export const constantRouterMap = [
 export default new Router({
   // mode: 'history', //后端支持可开
   scrollBehavior: () => ({y: 0}),
-  routes: constantRouterMap,
-  mode: 'history',
-  base: '/dist/'
+  routes: constantRouterMap
 })
 export const asyncRouterMap = [
   {
