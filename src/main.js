b65f320e9a17a30ff85a2befbd1dc0f9f93922f5
jack <1339326122@qq.com>
2019-10-10 22:42:27 +0800

    \\

--- src/main.js
+++ src/main.js
@@ -15,7 +15,7 @@ Vue.prototype.api = api
 //全局的常量
 Vue.prototype.hasPerm = hasPermission
 //生产环境时自动设置为 false 以阻止 vue 在启动时生成生产提示。
-Vue.config.productionTip = false
+Vue.config.productionTip = true
   // (process.env.NODE_ENV != 'production')
 new Vue({
   el: '#app',
